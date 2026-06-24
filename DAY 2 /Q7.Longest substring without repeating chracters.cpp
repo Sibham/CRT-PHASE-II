010d0fa2ebc4378fb2bb8f38df91a9fc0f7facc5
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-06-24 22:46:30 +0530

    Implement solution for longest substring without repeating characters

---
+++ DAY 2 /Q7.Longest substring without repeating chracters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> mp;
+        int i = 0, j = 0;
+        int ans = 0;
+        int n = s.length();
+
+        while(j < n) {
+            mp[s[j]]++;
+
+            // Case 1: window valid (all unique chars)
+            if(mp.size() == (j - i + 1)) {
+                ans = max(ans, j - i + 1);
+                j++;
+            }
+            // Case 2: window invalid (duplicate chars)
+            else if(mp.size() < (j - i + 1)) {
+                while(mp.size() < (j - i + 1)) {
+                    mp[s[i]]--;
+                    if(mp[s[i]] == 0) {
+                        mp.erase(s[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        return ans;
+    }
+};

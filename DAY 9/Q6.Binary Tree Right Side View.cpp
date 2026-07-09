f4a142ed79dcfeff49735164793afe8aa40fab2d
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-09 16:37:50 +0530

    Implement right side view of binary tree

---
+++ DAY 9/Q6.Binary Tree Right Side View.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root)
+            return {};
+        
+        vector<int> result;
+        queue<TreeNode*> que;
+        que.push(root);
+        
+        while(!que.empty()) {
+            int n = que.size();
+            TreeNode* rightNode = NULL;
+            while(n--) {
+                rightNode = que.front();
+                que.pop();
+                
+                if(rightNode->left)
+                    que.push(rightNode->left);
+                if(rightNode->right)
+                    que.push(rightNode->right);
+            }
+            result.push_back(rightNode->val);
+        }
+        return result;
+    }
+};

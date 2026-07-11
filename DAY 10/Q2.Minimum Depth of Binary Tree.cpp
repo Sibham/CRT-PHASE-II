d53f9dc673340f569ce1a179e19d08616eb58050
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-11 23:05:57 +0530

    Add BFS solution for minimum depth of binary tree
    Implemented BFS to find the minimum depth of a binary tree.

--- DAY 10/Q2.Minimum Depth of Binary Tree.cpp
+++ DAY 10/Q2.Minimum Depth of Binary Tree.cpp
@@ -15,3 +15,39 @@ public:
     }
 };
 
+//BFS
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+
+        queue<TreeNode*>q;
+        q.push(root);
+
+        int depth = 1;
+
+        while(!q.empty()){
+            int n = q.size();
+            while(n--){
+                TreeNode* temp = q.front();
+                q.pop();
+
+                if(temp->left==NULL && temp->right==NULL){
+                    return depth;
+                }
+                if(temp->left != NULL){
+                    q.push(temp->left);
+                }
+                if(temp->right != NULL){
+                    q.push(temp->right);
+                }
+            }
+            depth++;
+        }
+        return -1;
+
+    }
+};
+

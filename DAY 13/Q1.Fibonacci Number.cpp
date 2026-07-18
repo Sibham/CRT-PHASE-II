276ce6b1ce448316fd478bd37fe12da05048eb5f
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-18 23:03:30 +0530

    Create Q1.Fibonacci Number.cpp

---
+++ DAY 13/Q1.Fibonacci Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    vector<int>dp;
+public:
+int solve(int n){
+    if(n==0 || n==1){
+    return n ;
+    }
+    if(dp[n] != -1){
+        return dp[n];
+    }
+    return dp[n]=solve(n-1) + solve(n-2);
+}
+    int fib(int n) {
+        dp.resize(n+1,-1);
+        return solve(n);
+    }
+};

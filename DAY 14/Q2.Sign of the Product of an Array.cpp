6158b6d693565e41627dfc3cd525238c2bb4e1d4
Sibham Maity <163507491+Sibham@users.noreply.github.com>
2026-07-23 15:13:31 +0530

    Implement arraySign function to determine product sign
    Refactor arraySign function to count negative numbers and determine product sign.

--- DAY 14/Q2.Sign of the Product of an Array.cpp
+++ DAY 14/Q2.Sign of the Product of an Array.cpp
@@ -13,3 +13,16 @@ public:
         return sign;
     }
 };
+
+
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+       int negCount = 0 ;
+       for(int i = 0 ; i<nums.size() ; i++){
+        if(nums[i]==0) return 0;
+        if(nums[i]<0) negCount++;
+       }
+        return (negCount % 2 == 0) ? 1 : -1; 
+    }
+};
